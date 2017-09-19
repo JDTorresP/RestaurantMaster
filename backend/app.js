40294088f61cf69b865cb63788c5f037af4e0c83
Felipe Iregui C <30903412+Feireca@users.noreply.github.com>
2017-09-18 23:11:01 -0500

    codeReview

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+/*Felipe Iregui: La verdad no sé si cause algún error serio, pero yo diría que el proyecto está mal estructurado acá en github, esa carpeta backend no debería existir, simplemente aparecer ahí todas las carpetas, incluyendo este archivo. */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+/*Felipe Iregui: El users no se usa para nada sería mejor eliminarlo como hizo John*/
 var users = require('./routes/users');
 
 var app = express();
@@ -15,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
+/*Felipe Iregui: el favicon debería ir en frontend/public*/
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
